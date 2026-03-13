47a21ddc6c822f3c2235ecb38fe48920361f3611
C-Mac322 <caleb.mcdonald@umconnect.umt.edu>
2026-03-12 18:12:25 -0600

    Homework 9 files

---
+++ Homework 9/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  rect(275,400,75,125);
+  rect(225,475,175,125);
+  triangle(250,475,360,425,360,475)
+  circle(312,300,250)
+  triangle(175,320,188,280,190,320)
+  ellipse(250,280,60,30)
+  circle(227,280,15)
+  line(201,357,220,357)
+  point(225,357)
+  triangle(262,179,382,161,397,209)
+  triangle(211,157,211,226,319,175)
+  line(210,373,243,370)
+  line(243,370,227,351)
+  line(227,351,198,351)
+  line(194,341,230,344)
+  line(230,344,276,358)
+  line(276,358,260,414)
+  text("Caleb McDonald - In Shapes",10,580)
+}
